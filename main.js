8e0e2e0f6b5b706f40b714601145b6ba73791c8a
whoisalpheus <104221606+whoisalpheus@users.noreply.github.com>
2023-01-21 00:03:45 -0500

    Finish part 4 of tutorial

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
 // Make variables first to be able to use in events
 const form = document.getElementById('addForm');
 const itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
+// Delet event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 // Add item function
 function addItem(e) {
-    e.preventDefault();
+    e.preventDefault(); // So normal submission of form does not happen
 
-    console.log(1);
-};
+    // console.log(1);
+
+    // Get input value
+    const newItem = document.getElementById('item').value;
+
+    // Create new li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // console.log(li);
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create del button element
+    const deleteBtn = document.createElement('button');
+
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to list
+    itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e) {
+    if(e.target.classList.contains('delete')) {
+        if(confirm('Are you sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
+
+// Filter items
+function filterItems(e) {
+    // Convert text to lowercase
+    let text = e.target.value.toLowerCase();
+    // Get list
+    let items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+        let itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+}
